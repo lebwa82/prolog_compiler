e6b627efaec748a63ad5f5b62286d4fbfdb4630e
lebwa <lebwa82@gmail.com>
2022-11-26 18:23:53 +0300

    5

--- main.c
+++ main.c
@@ -17,6 +17,13 @@ enum type {
     STRUCTURA,
 };
 
+enum key {
+    START,
+    CONDITION,
+};
+
+
+
 typedef struct key_words {
     int index;
     char word[10];
@@ -27,6 +34,20 @@ key_words key_words_list[] = {{-1, "+"}, {-2, "-"}, {-3, "="}, {-4, "*"},
     {-9, "]"}, {-10, ":"}, {-11, ";"}, {-12, ","},
     {-13, "|"}, {-14, "/"}, {-15, ":-"}
 };
+
+char operators[] = {'>', '<'};
+int is_operator(char symbol)
+{
+    int len = sizeof(operators);
+    int i;
+    for(i=0; i<len; i++)
+    {
+        if(symbol == operators[i])
+            return 1;
+    }
+    return 0;
+}
+
 
 struct names {
     int index;
@@ -40,7 +61,7 @@ int is_key_word(char *word)
     return 0;
 }
 
-char stroka[] = "read(mark, Book)";
+char stroka[] = "read(mark,Book):-fact(Tom,Jerry):-X>5..";
 char *i = stroka - 1;
 
 char get_symbol()
@@ -65,10 +86,13 @@ char *get_word()
     char *word = malloc(30 * sizeof(char));
     //strcat(word, &symbol);
     char symbol = get_symbol();
+    int i = 0;
     while (isalpha(symbol) || isdigit(symbol) || symbol == '_') {
-        strcat(word, &symbol);
+        //strcat(word, &symbol);
+        word[i] = symbol; i++;
         symbol = get_symbol();
     }
+    word[i] = '\0';
     step_back();
     return word;
 }
@@ -85,6 +109,31 @@ char get_word_f()
     return 0;
 }
  
+float get_number() {
+        char symbol = get_symbol();
+        float number = 0;
+        while (isdigit(symbol) || number == 0) {
+            number = number * 10 + (symbol - '0');
+            symbol = get_symbol();
+        }
+        if (symbol == '.') {
+            float tmp = 10;
+            char symbol = get_symbol();
+            while (isdigit(symbol) || number == 0) {
+                number += (symbol - '0') / tmp;
+                tmp *= 10;
+                symbol = get_symbol();
+            }
+            printf("READ FLOAT\n");
+            
+        }
+        else
+            printf("READ INT\n"); 
+        
+        step_back();
+        printf("DIGIT = %f\n", number);
+        return number;
+    }
 
 
 int main()
@@ -92,7 +141,7 @@ int main()
     //f = fopen("input.txt", "r");
     char symbol = 0;
     //char word[30];
-
+    enum key key = START;
 START: {
         symbol = get_symbol();
         switch (symbol) {
@@ -105,7 +154,7 @@ START: {
                 return 0;
         }
         if (isalpha(symbol)) {
-            goto WORD;
+            goto RELATION_NAME;
         }
 
         printf("START ERROR\n");
@@ -151,7 +200,7 @@ MULTILINE_COMMENT: {
                 goto ERROR;
         }
     }
-WORD: {
+RELATION_NAME: {
         step_back();
         char *word = get_word();
         symbol = get_symbol();
@@ -164,7 +213,7 @@ WORD: {
                 goto PREDICT_RELATION_OR_FACT_FIRST_VARIABLE_OR_STRING;
             } else {
                 free(word);
-                printf("WORD ERROR\n");
+                printf("RELATION_NAME ERROR\n");
                 goto ERROR;
             }
         }
@@ -186,7 +235,7 @@ PREDICT_RELATION_OR_FACT_FIRST_VARIABLE_OR_STRING: {
         goto ERROR;
     }
 
-DEFINITELY_FACT_SOLE_STRING: {
+DEFINITELY_FACT_SOLE_STRING: { 
     step_back();
     char *word = get_word();
         switch (symbol) {
@@ -200,18 +249,36 @@ DEFINITELY_FACT_SOLE_STRING: {
 }
 
 PREDICT_RELATION_OR_FACT_FIRST_VARIABLE: {
-    step_back();
     char *word = get_word();
+    symbol = get_symbol();
     switch (symbol) {
         case ',':
             goto PREDICT_RELATION_SECOND_NAME;
         case ')':
-            goto PREDICT_POINT;
+            goto DEFINITELY_END_OF_FACT;
 
         default:
             printf("PREDICT_RELATION_OR_FACT_FIRST_VARIABLE ERROR\n");
             goto ERROR;
     }
+}
+
+DEFINITELY_END_OF_FACT: {
+    symbol = get_symbol();
+    if (symbol == '.') {
+        printf("FACT COMPLETE\n");
+        goto  START;
+    }
+    if(key == CONDITION)
+    {
+        if(symbol == ',')
+        {
+            printf("FACT COMPLETE, READING NEXT RELATION\n");
+            goto START_CONDITION_BODY;
+        }
+    }
+    printf("DEFINITELY_END_OF_FACT ERROR\n");
+    goto ERROR;
 }
 
 
@@ -248,29 +315,106 @@ PREDICT_RELATION_SECOND_NAME_AS_STRING: {
 PREDICT_RELATION_SECOND_NAME_AS_VARIABLE: {
         step_back();
         char *word = get_word();
-        step_back();
+        //step_back();
         goto PREDICT_RELATION_COMPLETE_PREDICT_BRACKET_AND_POINT;
     }
 
 PREDICT_RELATION_COMPLETE_PREDICT_BRACKET_AND_POINT: {
         symbol = get_symbol();
         if (symbol == ')') {
-            goto PREDICT_POINT;
+            goto PREDICT_POINT_OR_CONDITION;
         }
         printf("PREDICT_RELATION_COMPLETE_PREDICT_BRACKET ERROR\n");
         goto ERROR;
     }
 
-PREDICT_POINT: {
+PREDICT_POINT_OR_CONDITION: {
     symbol = get_symbol();
     if (symbol == '.') {
         printf("Sentence complete\n");
         goto START;
     }
-    printf("PREDICT_POINT ERROR\n");
+
+    if (symbol == ':') {
+        printf("RELATION COMPLETE READING CONDITION\n");
+        goto PREDICT_CONDITION_DASH; //ждем тире
+    }
+
+    if (key == CONDITION){
+        if(symbol == ','){
+            printf("RED CONDITION, READING NEXT CONDITION\n");
+            goto START_CONDITION_BODY;
+        } 
+    }
+    printf("PREDICT_POINT_OR_CONDITION ERROR\n");
     goto ERROR;
 }
 
+
+PREDICT_CONDITION_DASH: {//прочитали тире
+    symbol = get_symbol();
+
+    if (symbol == '-') {
+        key = CONDITION;
+        goto START_CONDITION_BODY; //ждем тире
+    }
+    printf("PREDICT_CONDITION_DASH ERROR\n");
+    goto ERROR;
+}
+
+START_CONDITION_BODY:{
+    symbol = get_symbol();
+    if(!isalpha(symbol))
+    {
+        printf("START_CONDITION_BODY_WORD ERROR\n");
+        goto ERROR;
+    }
+    step_back();
+    char *word = get_word();
+    //потенчиално определить это отношение или переменная
+
+    symbol = get_symbol();
+    if(is_operator(symbol)) {
+        goto CONDITION_BODY_SECOND_WORD_VARIABLE_OR_DIGIT;
+    }
+    if(symbol == '(') {
+        goto CONDITION_BODY_RELATION;//зацикливаемся
+    }
+    printf("START_CONDITION_BODY OPERATOR OR ( ERROR\n");
+    goto ERROR;
+    
+}
+
+CONDITION_BODY_SECOND_WORD_VARIABLE_OR_DIGIT:{
+    symbol = get_symbol();
+    if (isalpha(symbol)){
+        step_back();
+        char *word = get_word();
+        printf("RED OPERATOR EXPRESSION WITH A VARIABLE\n");
+        goto PREDICT_POINT_OR_CONDITION;
+    }
+
+    if (isdigit(symbol)){
+        step_back();
+        get_number();
+        printf("RED OPERATOR EXPRESSION WITH A DIGIT\n");
+        goto PREDICT_POINT_OR_CONDITION;
+        
+    }
+
+    printf("CONDITION_BODY_SECOND_WORD_VARIABLE_OR_DIGIT ERROR");
+    goto ERROR;
+}
+
+CONDITION_BODY_RELATION:{
+    symbol = get_symbol();
+    if (isalpha(symbol))
+    {
+        step_back();
+        goto PREDICT_RELATION_OR_FACT_FIRST_VARIABLE_OR_STRING;
+    }
+
+}
 
 
 ERROR:
@@ -278,26 +422,7 @@ ERROR:
     return 0;
 
 
-DIGIT: {
-        int int_number = 0;
-        while (isdigit(symbol) || int_number == 0) {
-            int_number = int_number * 10 + (symbol - '0');
-            symbol = get_symbol();
-        }
-        if (symbol == '.')
-            goto FLOAT;
-        else {}
-        //что-то еще
 
-FLOAT:
-        float float_number = int_number;
-        float tmp = 10;
-        while (isdigit(symbol) || int_number == 0) {
-            float_number += (symbol - '0') / tmp;
-            tmp *= 10;
-            symbol = get_symbol();
-        }
-    }
 
 VARIABLE:
     symbol = get_symbol();
@@ -311,7 +436,5 @@ VARIABLE:
 //комментарии для души
 //isdigit
 //isalpha
-//код хуйня, кодеры пидоры
-
 
 
