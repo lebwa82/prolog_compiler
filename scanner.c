2e71c6f055bb134e14423b007edf50e81381f817
lebwa <lebwa82@gmail.com>
2022-12-10 21:00:07 +0300

    add header.h to scaner

--- scanner.c
+++ scanner.c
@@ -3,54 +3,18 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include "header.h"
 
-typedef struct key_words {
-    int index;
-    char word;
-} key_words;
-
-key_words key_words_list[] = {{-1, '+'}, {-2, '-'}, {-3, '='}, {-4, '*'},
-    {-5, '/'}, {-6, '<'}, {-7, '>'}, {-8, '['},
-    {-9, ']'}, {-10, ':'}, {-11, ';'}, {-12, ','},
-    {-13, '|'}, {-14, '/'}, {-15, '('}, {-16, ')'}, {-17, '.'}, //{-18, '\0'}  //{-15, ":-"}, (-10,-2 = if)
-};
-
-int len_key_words_list = sizeof(key_words_list) / sizeof(key_words_list[0]);
-int is_key_word(char word)
-{
-    for (int i = 0; i < len_key_words_list; i++) {
-        //printf("%d %c\n", key_words_list[i].index, key_words_list[i].word);
-        if (key_words_list[i].word == word)
-            return key_words_list[i].index;
-    }
-    return 0;
-}
 
 int main_massiv_id[100];
 int main_i;
-
-enum type { //тип, положительный
-    DIGIT,
-    STRING,
-    NAME,
-    VAR_TYPE,
-};
 
 enum const_or_var {//вид
     CONSTANTA,
     VAR,
 };
 
-struct literal { //может как ключевое слово, так и литералы
-    int id;
-    enum type type;//тип
-    enum const_or_var const_or_var;//вид
 
-    char literal_name[30];//изначальное имя
-    char value_char[30]; //значение переменной
-    int value_int;
-    struct literal *next;
-};
 char pre_stroka[] = "";
 char stroka[100] = "nested_condition(\"mark\",Book):-relation(Tom,123,-22222).";
 int stroka_i = -1;
@@ -258,6 +222,7 @@ PREDICT_END_MULTILINE_COMMENT:{
     }
 */
 
+PRE_START:
     //считаем, что избавились от пробелов и комментариев
     stroka_i = -1;
 START:
